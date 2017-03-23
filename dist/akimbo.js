916e765f0580f9787622945d1e4a695a488fc5de
Wancieho <wancieho@gmail.com>
2017-03-23 12:50:06 +0200

    update Config to allow for namespacing

--- dist/akimbo.js
+++ dist/akimbo.js
@@ -287,7 +287,25 @@ var akimbo = {};
 
 	Config.prototype = {
 		get: function (config) {
-			return akimbo.App.Config[config];
+			//requested config has a . means we dont just get the root object the request is looking through a hierarchial object
+			if (config.indexOf('.') !== -1) {
+				var pieces = config.split('.');
+				var namespace = akimbo.App.Config;
+
+				//build namespace into object
+				for (var i = 0; i < pieces.length; i++) {
+					if (namespace[pieces[i]] === undefined) {
+						throw '"' + config + '" config does not exist';
+						break;
+					}
+
+					namespace = namespace[pieces[i]];
+				}
+
+				return namespace;
+			} else {
+				return akimbo.App.Config[config];
+			}
 		}
 	};
 })(akimbo);
