f234cb57cdaa5e0324d65d19ce0f10ab6dc1845e
Wancieho <wancieho@gmail.com>
2017-04-10 16:16:31 +0200

    forgot build

--- dist/akimbo.js
+++ dist/akimbo.js
@@ -28,7 +28,7 @@ var akimbo = {};
 			alert('history.pushState() not supported.');
 		}
 
-		if (instance === null) {
+		if (instance === null && runningTests !== true) {
 			instance = this;
 			instance.router = new Akimbo.Router();
 
@@ -274,8 +274,7 @@ var akimbo = {};
 
 	var listeners = [];
 
-	function Event() {
-	}
+	function Event() {}
 
 	Event.prototype = {
 		listen: function (event, callback, object) {
@@ -679,5 +678,44 @@ var akimbo = {};
 		}
 
 		this.event.listen(event, callback, object !== undefined && object !== null ? $.extend({}, this, object) : this);
+	};
+})(akimbo);
+(function (Akimbo) {
+	Akimbo.Test = Test;
+
+	function Test() {}
+
+	Test.prototype = {
+		component: function (classzor) {
+			var component = new classzor();
+
+			if (component.meta === undefined) {
+				throw '"' + component.name + '" meta property must be defined';
+			}
+
+			if (component.meta.constructor === undefined) {
+				throw '"' + component.name.constructor + '" constructor must be defined';
+			}
+
+			$.ajaxSetup({async: false});
+
+			$('body').remove('[' + component.meta.selector + ']').append('<div ' + component.meta.selector + ' style="display:none"></div>');
+
+			$('[' + component.meta.selector + ']').load('../' + component.meta.templateUrl + '?' + new Date().getTime(), function () {});
+
+			$.ajaxSetup({async: true});
+
+			component.getDefaultInstance = function () {
+				return component;
+			};
+
+			component.constructor(component);
+
+			if (component.listeners !== undefined) {
+				component.listeners(component);
+			}
+
+			return component;
+		}
 	};
 })(akimbo);
